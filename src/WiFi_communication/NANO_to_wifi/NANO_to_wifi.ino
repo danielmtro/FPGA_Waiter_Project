5e02de7767a628422a804bf54a81e9831561e349
AdamRiesel <89631388+AdamRiesel@users.noreply.github.com>
2024-10-21 22:24:58 +1100

    all code for arduino is commented and clean. Improvements needed

--- src/WiFi_communication/NANO_to_wifi/NANO_to_wifi.ino
+++ src/WiFi_communication/NANO_to_wifi/NANO_to_wifi.ino
@@ -2,62 +2,73 @@
 
 const char* ssid = "KIRBYBOTS";      // Your Wi-Fi network SSID
 const char* password = "room330!";  // Your Wi-Fi network password
-
+bool rcv_image_flag = false;
 uint16_t pixel;
 
+//delcare a pin number for sending info TO the FPGA GPIO
+int READY_OUT = 4;
 
-
-WiFiServer server(80);   // Create a server on port 80
+// Create a server on port 80
+WiFiServer server(80);   
 
 void setup() {
-  Serial.begin(9600);  // Communication between Arduino and WiFi
+  // Communication between Arduino and Serial monitor in IDE
+  Serial.begin(115200);  
+
+  //Setup communication from arduino to WIFI
+  // Start the server
   connectToWiFi(ssid, password);
-  server.begin();        // Start the server
+  server.begin();        
+
+  
+  //communication between FPGA and arduino
+  //Note: we cannot use Serial1.write because the FPGA doesn't have UART_RX
+  Serial1.begin(115200, SERIAL_8N1); //communication between FPGA and arduino
+
+  //declare READY_OUT as output pin
+  pinMode(READY_OUT, OUTPUT); 
 }
 
-void loop() {
-  WiFiClient client = server.available();  // Check for incoming clients
 
+void loop() {
+
+  // Check for incoming clients
+  WiFiClient client = server.available();  
+  
   if (client) {
     Serial.println("Client connected");
     delay(1000);
-    while (client.connected()) {
-      if (client.available() > 0){
-        String request = client.readStringUntil('\n');
-        if (request == "SEND_DATA"){
 
-          if(Serial.available() >= 2){
-            pixel = receive_UART();
-            // pixel = Serial.read();
-            client.write((uint8_t)(pixel >> 8));  // Send high byte
-            client.write((uint8_t)(pixel & 0xFF)); // Send low byte
-          }
-          else{
-            client.write("NULL");
-          }
-          
+    while (client.connected()) {
+      //if there is a message (request) being received over WiFI 
+      if (client.available() > 0){
+        //read the entire message
+        //NOTE: 1 character messages have been used for speed
+        String request = client.readStringUntil('\n');
+
+        //remove everything from the incoming buffer from the FPGA
+        //buffer size is only 256 bytes which is half as many pixels
+        if (request == "F"){
+          Serial1.flush();
+        }
+
+        //read 1 pixel off the Serial1 buffer and send it over WiFi
+        else if (request == "S"){        
+            if(Serial1.available() >= 2){              
+              pixel = receive_pixel();
+
+              //send pixel over WiFi
+              //NOTE: this is the slow part
+              //TODO: Can you please sanity the high byte send? 
+              //I suspect it should actually by >> 4 considering the data in
+              //pixel is only 12 bits not actually 16
+              client.write((uint8_t)(pixel >> 8));  // Send high byte
+              client.write((uint8_t)(pixel & 0xFF)); // Send low byte
+            }
         }
       }
-      //-------------------
-      // if (Serial.available() >= 2) {  // Wait until at least two bytes are available
-      //     Serial.println("pixel available");
-      //     pixel = receive_UART();
-
-
-      //     client.write((uint8_t)(pixel >> 8));  // Send high byte
-      //     client.write((uint8_t)(pixel & 0xFF)); // Send low byte
-
-      //     Serial.println("data sent to PC over wifi");
-      //   }
-      //-------------------OL' FAITHFUL----------------
-      // for (int i = 0; i < 320 * 240; i++) {
-      //   uint16_t pixel = getPixelData(i);  // Function to get pixel data
-      //   client.write((uint8_t)(pixel >> 8));  // Send high byte
-      //   client.write((uint8_t)(pixel & 0xFF)); // Send low byte
-      //   Serial.println("sent data");
-      // }
-      
     }
+    Serial.println("Dropping client");
     client.stop();
   }
 }
