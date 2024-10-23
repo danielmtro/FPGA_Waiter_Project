46a6d0ebcd2f124b89baa4101a6af86e5336e648
danielmtro <Daniel.Monteiro2205@gmail.com>
2024-10-24 00:17:20 +1100

    got colour detection working

--- src/ColourDetection/colour_detect.sv
+++ src/ColourDetection/colour_detect.sv
@@ -1,49 +1,60 @@
-/*
-
-Module takes in a pixel stream. Keeps listening to the pixel stream
-for 320 x 240 and then outputs a signal if the amount of red was above
-a threshold.
-
-*/
-
-
-module colour_detect #(
-    parameter THRESHOLD = 1152000 // about 50% of the colour in the image
-)(
-
-    input clk,
-    input [11:0]pixel,
-    input sop,  // lets us know when a frame starts and end
-    input eop,
-    output colour_flag
-
+module colour_detect(
+	input clk,
+	input [11:0] data_in,
+	input [3:0] upper_thresh,
+	input [16:0] address,
+	input sop,
+	output [11:0] data_out,
+	output [16:0] red_pixels
+	
 );
 
-    logic [3:0] red, green, blue;
-    logic cflag = 0;
+	logic [3:0] r;
+	logic [3:0] g;
+	logic [3:0] b;
+	
+	assign r = data_in[11:8];
+	assign g = data_in[7:4];
+	assign b = data_in[3:0];
+	
+	
+	// Prototype a counter
+	logic [16:0] prev_address;
+	logic [16:0] red_pixel_count;
+	
+	// create an appropriate counter for red pixels
+	always_ff @(posedge clk) begin
+		prev_address <= address;
+		
+		if(sop) begin
+			red_pixel_count <= 0;
+		end
+		else if(prev_address != address) begin
+			
+			// increment red pixel count
+			if(is_red) begin
+				red_pixel_count <= red_pixel_count + 1;
+			end
+		end
+	end
+	
+	// signals based on thresholding
+	logic is_red;
+	assign is_red = (r > g) & (r > b) & (r >= upper_thresh);
 
-    assign red = pixel[11:8];
-    assign green = pixel[7:4];
-    assign blue = pixel[3:0];
-
-    // determine how the count should increment
-    integer count = 0;
-    always_ff @(posedge clk) begin
-        if(sop) begin
-            count <= 0;
-        end
-        else begin
-            count <= count + red;
-        end
-    end
-
-    // determine what the colour flag should be set as
-    always_ff @(posedge clk) begin
-        if(eop) begin
-            cflag = (count > THRESHOLD) ? 1'b1 :1'b0;
-        end
-    end
-    
-    assign colour_flag = cflag;
-
+	// determine output based on colours
+	always_comb begin
+		// determine if r is dominant
+		if(is_red) begin
+			data_out = {4'b1111, 8'b00000000};
+		end	
+		else begin
+			data_out = 12'b000000000000;
+		end
+	end
+	
+	
+	assign red_pixels = red_pixel_count;
+	
+	
 endmodule
